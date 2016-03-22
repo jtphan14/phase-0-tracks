e1bf228a46d14407fc6e2432897677f2a0ffdc29
Jacqueline Phan <jtphan14@gmail.com>
2016-03-22 16:26:27 -0700

    Initial Commit

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year_born = gets.chomp.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp.to_i
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp.to_i
