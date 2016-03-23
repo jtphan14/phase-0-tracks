f5ca07985dc9715b3ee55157574f65ebec23330c
Jacqueline Phan <jtphan14@gmail.com>
2016-03-22 21:34:46 -0700

    Complete Relase 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,66 +1,75 @@
-puts "How many user's will be processed?"
+num = 0
+
+puts "How many users do you want to input"
 users = gets.chomp.to_i
 
-until 
+def employee_vampire_verification
 
-puts "What is your name?"
-name = gets.chomp
+    puts "What is your name?"
+    name = gets.chomp
 
-if name == "Drake Cula"|| name == "Tu Fang"
-    vampire_name = true
-else
-    vampire_name = false
+        if name == "Drake Cula"|| name == "Tu Fang"
+            vampire_name = true
+        else
+            vampire_name = false
+        end
+
+    puts "How old are you?"
+    age = gets.chomp.to_i
+
+    puts "What year were you born?"
+    year_born = gets.chomp.to_i
+
+    calculated_age = 2016 - year_born
+    age_correct = age == calculated_age
+
+    puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+    order = gets.chomp.downcase
+
+    until order == "yes" || order == "no"
+        puts "Please answer yes or no. Our company cafeteria serves garlic bread. Should we order some for you?"
+        order = gets.chomp.downcase        
+    end
+
+    if order == "yes"
+        garlic_bread = true
+    else 
+        garlic_bread = false
+    end
+
+    puts "Would you like to enroll in the company's health insurance?"
+    enroll = gets.chomp
+
+    until enroll == "yes" || enroll == "no"
+        puts "Please answer yes or no. Would you like to enroll in the company's health insurance?"
+        order = gets.chomp.downcase        
+    end
+
+    if enroll == "yes"
+        health_insurance = true
+    else
+        health_insurance = false
+    end
+
+
+    if (vampire_name==false) && (age_correct == true) && (garlic_bread == true || health_insurance == true) == true
+    	puts "Probably not a vampire"
+    elsif (vampire_name==false)  && (age_correct == false) && (garlic_bread == false) && (health_insurance == true) == true
+    	puts "Probably a vampire"
+    elsif (vampire_name==false)  && (age_correct == false) && (garlic_bread == true) && (health_insurance == false) == true
+        puts "Probably a vampire"
+    elsif (vampire_name==false)  && (age_correct == false) && (garlic_bread == false) && (health_insurance == false) == true
+        puts "Almost certainly a vampire"
+    elsif  (vampire_name==true) 
+        puts "Definitely a vampire"
+    else
+    	puts "Results inconclusive"
+    end
+
 end
 
-puts "How old are you?"
-age = gets.chomp.to_i
-
-puts "What year were you born?"
-year_born = gets.chomp.to_i
-
-calculated_age = 2016 - year_born
-age_correct = age == calculated_age
-
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-order = gets.chomp.downcase
-
-until order == "yes" || order == "no"
-    puts "Please answer yes or no. Our company cafeteria serves garlic bread. Should we order some for you?"
-    order = gets.chomp.downcase        
-end
-
-if order == "yes"
-    garlic_bread = true
-else 
-    garlic_bread = false
-end
-
-puts "Would you like to enroll in the company's health insurance?"
-enroll = gets.chomp
-
-until enroll == "yes" || enroll == "no"
-    puts "Please answer yes or no. Would you like to enroll in the company's health insurance?"
-    order = gets.chomp.downcase        
-end
-
-if enroll == "yes"
-    health_insurance = true
-else
-    health_insurance = false
-end
-
-
-if (vampire_name==false) && (age_correct == true) && (garlic_bread == true || health_insurance == true) == true
-	puts "Probably not a vampire"
-elsif (vampire_name==false)  && (age_correct == false) && (garlic_bread == false) && (health_insurance == true) == true
-	puts "Probably a vampire"
-elsif (vampire_name==false)  && (age_correct == false) && (garlic_bread == true) && (health_insurance == false) == true
-    puts "Probably a vampire"
-elsif (vampire_name==false)  && (age_correct == false) && (garlic_bread == false) && (health_insurance == false) == true
-    puts "Almost certainly a vampire"
-elsif  (vampire_name==true) 
-    puts "Definitely a vampire"
-else
-	puts "Results inconclusive"
+until num >= users
+    employee_vampire_verification
+    num +=1
 end
 
