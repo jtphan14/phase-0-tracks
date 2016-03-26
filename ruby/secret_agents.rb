0ec64c0b6ccbbc1acfa3045be454a6000c104504
Jacqueline Phan <jtphan14@gmail.com>
2016-03-26 11:48:35 -0700

    Add basic encryption and decryption without conditionals

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,12 +1,9 @@
-# testing
-​
-#  create a method called encrypt that takes in a string parameter
-#  get the length of the string, and set the index
-#  go through each letter with a loop in the string and forward it by one letter;
-#  return the lowercase string
-#  call the method and pass the string
-​
-​
+# Create a method called encrypt that takes in a string parameter
+# Get the length of the string, and set the index
+# Go through each letter with a loop in the string and forward it one letter
+# Return the lower case string
+# Call the method and pass the string
+
 def encrypt(password)
     index = 0
     while index < password.length
@@ -15,44 +12,32 @@ def encrypt(password)
     end
     p password
 end
-​
-# encrypt("abc")
-​
-​
-​
-#  create decrypt method that takes previous encrypted string
-#  get length of string and set the index
-#  find position of letter in alphabet
-#  subtract one from the previous position
-#  return lower case string
-#  call the method and pass encrypt value
-​
-def previous
+
+
+
+# Create decrypt method that takes previous encrypted string
+# Get lenghts of string and set the index
+# Find position of letter in alphabet
+# Subtract one from the previous position
+# Return lower case string
+# Call the method and pass encrypt value
+
+def previous(x)
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    puts alphabet[0]
+    previous_letter_index = alphabet.index(x) - 1
+    previous_letter = alphabet[previous_letter_index]
 end
-​
+
 def decrypt(password)
     index = 0
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-​
     while index < password.length
-        password[index] = password[index]
+        password[index] = previous(password[index])
         index += 1
     end
-    p password
-​
+p password
 end
-​
-​
-encrypt(decrypt("bcd"))
-​
-# puts "\n"
-​
-decrypt(encrypt("bcd"))
-​
-# puts "\n"
-​
-# previous(encrypt("bcd"))
-​
-​
+
+encrypt ("abc")
+encrypt ("z")
+decrypt("bcd")
+decrypt("afe")
