b9b08466d1cb0dba1cf980eca81be3144e1a4a69
Jacqueline Phan <jtphan14@gmail.com>
2016-03-26 14:18:37 -0700

    Update conditionals and add driver code

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -6,10 +6,14 @@
 
 def encrypt(password)
     index = 0
+    password.downcase!
     while index < password.length
         if password[index] == "z"
         password[index] = "a"
         index+=1
+        elsif password[index] == " "
+            password[index] = " "
+        index +=1
         else
         password[index] = password[index].next
         index += 1
@@ -29,20 +33,45 @@ end
 
 def previous(x)
     alphabet = "abcdefghijklmnopqrstuvwxyz"
-    previous_letter_index = alphabet.index(x) - 1
+    previous_letter_index = alphabet.index(x)- 1
     previous_letter = alphabet[previous_letter_index]
 end
 
 def decrypt(password)
+    password.downcase!
     index = 0
     while index < password.length
-        password[index] = previous(password[index])
+        if password[index] == " "
+        password[index] = " "
+        index+=1
+        else password[index] = previous(password[index])
         index += 1
+        end
     end
-p password
+    p password
 end
 
-encrypt ("abc")
-encrypt ("zed")
-decrypt("bcd")
-decrypt("afe")
+#OUR DRIVER CODE
+
+#Ask agent if they want to use encrypt/decrypt password
+puts "Type 1 if you would like to encrypt your password or type 2 if you would like to decrypt your password."
+#Retrieve answer
+answer = gets.chomp
+until answer == "1" || answer == "2"
+ puts "Type 1 if you would like to encrypt your password or type 2 if you would like to decrypt your password."
+ answer = gets.chomp
+end
+
+#Ask agent for password
+puts "Please type in your password"
+password = gets.chomp
+#Create until loop to confirm answer is either 1 or 2
+
+#Create if/else statement to decrypt or encrypt password and print results
+
+    if answer == "1"
+        encrypt(password)
+    else answer == "2"
+        decrypt(password)
+    end
+
