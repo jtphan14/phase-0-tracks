9c313760a1e11dd52875bc39280b3599cd017942
Jacqueline Phan <jtphan14@gmail.com>
2016-03-29 21:37:33 -0700

    add input data and convert data into hash ruby hashes.rb

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,8 +1,49 @@
-#Ask designer for information regarding the client's name, age, number of children, decor theme, favorite color, dislikes.
+#Ask designer for information regarding the client's name, age, number of children, decor theme, favorite color, likes flowers, likes open spaces.
+#Converts answer into variable
+puts "Clients Name"
+name = gets.chomp.to_s
 
-#Gather input and save as symbol
+puts "Clients Age"
+age = gets.chomp.to_i
 
-#Add symbols into hash
+puts "Number of Children"
+children = gets.chomp.to_i
+
+puts "Decor Theme"
+decor = gets.chomp.to_s
+
+puts "Favorite Color"
+favorite_color = gets.chomp.to_s
+
+
+puts "Likes Flowers"
+likes_flowers = gets.chomp
+#Create if/else statement to convert string to boolean
+if likes_flowers == "yes"
+		likes_flowers = true 
+	else 
+		likes_flowers == "no"
+		likes_flowers = false
+end
+
+
+
+puts "Likes Open Spaces"
+space = gets.chomp
+if space == "yes"
+		space = true 
+	else 
+		flowers == "no"
+		space = false
+end
+
+puts "sample"
+sample = gets.chomp
+
+
+#Add variables into hash, and automatically converts into symbols. 
+info = {Name: name, Age: age, Children: children, Decor: decor, Favorite_Color: favorite_color, Likes_Flowers: likes_flowers, Likes_Space: space, Sample: sample }
 
 #Print out hash
 
+p info
