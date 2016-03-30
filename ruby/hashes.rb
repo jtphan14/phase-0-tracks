1495daff720fa61ce096ec6280d7fa651d2b29cb
Jacqueline Phan <jtphan14@gmail.com>
2016-03-30 11:25:10 -0700

    Add option for designer to update value

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -30,20 +30,43 @@ end
 
 puts "Likes Open Spaces"
 space = gets.chomp
+#Create if/else statement to convert string to boolean
 if space == "yes"
 		space = true 
 	else 
-		flowers == "no"
+		space == "no"
 		space = false
 end
 
-puts "sample"
-sample = gets.chomp
 
+#Add variables into hash and  converts into symbols. 
+info = {
+	Name: name, 
+	Age: age, 
+	Children: children, 
+	Decor: decor, 
+	Favorite_Color: favorite_color, 
+	Likes_Flowers: likes_flowers, 
+	Likes_Space: space, 
 
-#Add variables into hash, and automatically converts into symbols. 
-info = {Name: name, Age: age, Children: children, Decor: decor, Favorite_Color: favorite_color, Likes_Flowers: likes_flowers, Likes_Space: space, Sample: sample }
+}
 
 #Print out hash
+p info
 
+#Ask designer if they want to make any changes
+puts "Did you have any changes you wanted to make? Type none if not, type in the name of the category if so."
+new_change = gets.chomp.to_sym
+#Create if/else statement
+#If answer is none. We end the program
+if new_change == :none
+	p "Thank you for your submission!"
+#Get updated value if needed otherwise.
+else 
+	puts "What would you like to update this information to?"
+	new_value = gets.chomp
+end
+#Update new value to hash
+info[new_change] = new_value
+#Reprint Hash
 p info
