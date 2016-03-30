2e717f56a0de6739b9c43a4feb4d4703b5d6543f
Jacqueline Phan <jtphan14@gmail.com>
2016-03-29 20:21:31 -0700

    Initial Commit

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+#Ask designer for information regarding the client's name, age, number of children, decor theme, favorite color, dislikes.
+
+#Gather input and save as symbol
+
+#Add symbols into hash
+
+#Print out hash
+
