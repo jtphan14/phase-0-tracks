1ff75c9f4bc649ab17467ab6cb1aee4b220628be
Jacqueline Phan <jtphan14@gmail.com>
2016-03-31 16:57:08 -0700

    Initial Commit

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,38 @@
+food_court = {
+	mcdonalds: {
+		meals {
+			Number_One: {
+				items_inc: ['Big Mac', 'French Fries', 'Drink'],
+				price: 6,
+			}
+			Number_Two: {
+				items_inc: ['20 piece chicken nuggets', 'French Fries', 'Drink']
+				price: 5,
+			}
+			
+		}
+		value_menu_prices  {
+			#assuming $USD
+			hamburger: 1,
+			double_cheeseburger: 2,
+			ice_cream: 1.05,
+		}
+	}
+	panda_express: {
+		meals {
+			Combo_1: {
+				items_inc: ['Fried Rice', 'Orange Chicken']
+				price: 6
+			}
+
+			Combination_2: ['Fried Rice', 'Brocolli Beef']
+		}
+
+
+	}
+	hot_dog:
+	{
+
+	}
+}
+
