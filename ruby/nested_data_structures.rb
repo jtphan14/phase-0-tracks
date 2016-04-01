526ac9fb6c2394a2d08b9a9ec5d22a638c4dfc32
Jacqueline Phan <jtphan14@gmail.com>
2016-03-31 17:25:26 -0700

    Complete Nested Data Structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,38 +1,46 @@
 food_court = {
-	mcdonalds: {
-		meals {
-			Number_One: {
+	mcdonalds:{
+		meals:{
+			number_one:{
 				items_inc: ['Big Mac', 'French Fries', 'Drink'],
 				price: 6,
-			}
-			Number_Two: {
-				items_inc: ['20 piece chicken nuggets', 'French Fries', 'Drink']
+			},
+			number_two:{
+				items_inc:['20 piece chicken nuggets', 'French Fries', 'Drink'],
 				price: 5,
-			}
-			
-		}
-		value_menu_prices  {
+			}	
+		},
+		value_menu_prices:{
 			#assuming $USD
 			hamburger: 1,
 			double_cheeseburger: 2,
 			ice_cream: 1.05,
 		}
-	}
-	panda_express: {
-		meals {
-			Combo_1: {
-				items_inc: ['Fried Rice', 'Orange Chicken']
+	},
+	
+	panda_express:{
+		meals:{
+			Combo_1:{
+				items_inc:['Fried Rice', 'Orange Chicken'],
+				price: 6
+			},
+			Combination_2:{
+				items_inc:['Fried Rice', 'Brocolli Beef'],
 				price: 6
 			}
-
-			Combination_2: ['Fried Rice', 'Brocolli Beef']
+		},
+		a_la_carte_prices:{
+			chow_mein: 3,
+			drink: 2,
 		}
-
-
+	},
+	
+	hot_dog_on_a_stick:{
+		menu_price:{
+			corn_dog: 3,
+			fries: 2,
+			lemonade: 2
+		},
+		lemonade_flavors:['original', 'strawberry', 'kiwi']
 	}
-	hot_dog:
-	{
-
-	}
-}
-
+}	
