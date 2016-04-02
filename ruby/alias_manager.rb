bbb7d950ca5fec8ee253f33fa0474fceb04c2469
Jacqueline Phan <jtphan14@gmail.com>
2016-04-02 13:05:11 -0700

    Add Pseudocode. Initial Commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+#Asks for Users Name
+
+#Gets Users Name
+
+#Define and use method to swap the first and last name
+
+#Convert String into Array
+
+#Confirm if letter is a vowel
+
+#If the letter is a vowel, we need to change to the next vowel. 
+def vowel (letter)
+	vowels = "aeiou"
+	vowel_idx = vowels.index(letter)
+	vowels[vowels_ix +1]
+end
+
+#If consonant, we need to change to the next consonant.
+
+#Print out program
