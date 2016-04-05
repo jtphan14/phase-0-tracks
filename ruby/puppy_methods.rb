d76945a087a224c3c73b4906a86fc83fabc2eeab
Jacqueline Phan <jtphan14@gmail.com>
2016-04-05 14:14:00 -0700

    Add dog years method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -13,9 +13,14 @@ class Puppy
   	puts "Rolls Over"
   end
 
+  def dog_years(i)
+  	puts (i)*7 
+  end
+
 end
 
 fido = Puppy.new
 fido.fetch("ball")
 fido.speak(3)
 fido.roll_over
+fido.dog_years(3)
