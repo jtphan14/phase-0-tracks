525083a7641ffff12139da60eeff86098de0e456
Jacqueline Phan <jtphan14@gmail.com>
2016-04-05 14:58:05 -0700

    Add Cats Class and complete methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -35,3 +35,79 @@ fido.dog_years(3)
 fido.bang
 
 spot = Puppy.new
+
+class Puppy
+
+ def fetch(toy)
+   puts "I brought back the #{toy}!"
+   toy
+ end
+
+def speak(i)
+     (i).times {puts "Woof"}
+ end
+
+ def roll_over
+     puts "Rolls Over"
+ end
+
+ def dog_years(i)
+     puts (i)*7 
+ end
+
+ def bang
+     puts "Bang! (Dog rolls on side)"
+ end
+
+ def initialize
+     puts "Initializing new puppy instance..."
+ end
+
+end
+
+fido = Puppy.new
+fido.fetch("ball")
+fido.speak(3)
+fido.roll_over
+fido.dog_years(3)
+fido.bang
+
+spot = Puppy.new
+
+index = 0
+
+
+class Cats
+
+ def initialize
+     puts "Initializing new cats instance..."
+ end
+
+ def speak(i)
+     (i).times {puts "Meow"}
+ end
+
+ def sleep
+     puts "Look at this cat sleep"
+ end
+
+end
+
+cats_array=[]
+
+number = 0
+
+until number == 50 do 
+    number += 1
+    name = Cats.new
+    p name
+    cats_array << name
+end
+
+cats_array.each {|cat| cat.sleep}
+cats_array.each {|cat| cat.speak(3)}
+
+begin garfield = Cats.new
+garfield.speak(2)
+garfield.sleep
+end
