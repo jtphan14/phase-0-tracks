48ebd15ac3e871dcbfae74b5b8a4feae01af8655
Jacqueline Phan <jtphan14@gmail.com>
2016-04-06 16:19:35 -0700

    Add Pseudocode

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,1 +1,16 @@
 #6.4 Solo Challenge: Designing and Implementing a Class
+
+#Create Class: Student
+
+#Create initialize method and define attributes.
+#Attribute #1: String: Name of Student
+#Attribute #2: Integer: Age of Student
+#Attribute #3: Boolean: Honors?
+
+#Method #1: Completes homework
+
+#Method #2: Goes to (School)
+
+#Method #3: Eats (lunch)
+
+#End Class
