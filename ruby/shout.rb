25d4cf0d302f029ba52274385d0bc67259b050d1
Jacqueline Phan <jtphan14@gmail.com>
2016-04-07 15:37:23 -0700

    Add a standalone module to a mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,8 +1,11 @@
 #6.5 Modules
 
-#Declare Module
+=begin 
+Release 0
 
+#Declare Module
 module Shout
+#Declare Methods using "Self" Keyword
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
 	end
@@ -11,6 +14,34 @@ module Shout
 		words + "!!!" + " :)"
 	end
 end
-
+#Driver Code
 p Shout.yell_angrily("Ah")
 p Shout.yelling_happily("Yay")
+=end
+
+#Release 3: Convert a Standalone Module to a Mixin
+
+#Declare Module
+module Shout
+#Method
+	def cheer(team_name)
+		puts "Go #{team_name}!!!"
+	end
+end
+
+#Define Class
+class Cheerleader
+	#include module
+	include Shout
+end
+#Define Class
+class Fans
+	#include module
+	include Shout
+end
+
+cheerleader = Cheerleader.new
+cheerleader.cheer("Chargers")
+
+fans = Fans.new
+fans.cheer("Warriors")
