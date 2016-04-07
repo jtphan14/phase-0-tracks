157a00f4d8d60465cd387cfb59afce85919bb908
Jacqueline Phan <jtphan14@gmail.com>
2016-04-07 15:25:59 -0700

    add methods to modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -3,5 +3,14 @@
 #Declare Module
 
 module Shout
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
 
+	def self.yelling_happily(words)
+		words + "!!!" + " :)"
+	end
 end
+
+p Shout.yell_angrily("Ah")
+p Shout.yelling_happily("Yay")
