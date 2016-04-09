741af2ed79738a580eea5c626721ad5ff4bfdc7b
Jacqueline Phan <jtphan14@gmail.com>
2016-04-08 18:02:28 -0700

    Complete Initialize Method

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,3 +1,13 @@
 class TodoList
+#Create Initialize Method	
+attr_accessor :get_items
+
+	def initialize (get_items)
+		@get_items = get_items
+	end
+
 
 end
+
+p list = TodoList.new(["do the dishes", "mow the lawn"])
+p list.get_items
