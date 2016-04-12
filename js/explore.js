2fbeb0506679560390cd1e561b7b9efc317deee9
Jacqueline Phan <jtphan14@gmail.com>
2016-04-11 17:01:35 -0700

    Add reverse string code and driver code

--- js/explore.js
+++ js/explore.js
@@ -1,15 +1,16 @@
 //Create Function
 
-//Create a loop that will start from the first letter and move to the last.
-//Loop will start off at 1 and will start by printing the first letter of the loop into a new string
-//On the second loop, the letter will be printed to the left of the new string.
-//The loop will continue until it has reached the end of the word 
-//ie. when the amount of loops = amount of letters in the word.
+//Create a loop that will start from the last letter and move to the left
+//Loop will start off at the last letter and will reprint the letter into a new variable
+//On the second loop, the letter will be added to the new variable string
+//The loop will continue until it reaches the first letter
 
+
+reverseString = ""
 function reverse(string) {
-	for (var i = 0; i < string.length; i++) {
-		console.log(string[i])
+	for (var i = (string.length-1) ; i >= 0; i--) {reverseString = reverseString + string[i]
 	}
+	console.log(reverseString)
 }
-
-reverse("hello")
+//driver code
+reverse("hola")
