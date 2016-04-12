69eb31d738fca3c680cc45f0cc2d8d562fb40153
Jacqueline Phan <jtphan14@gmail.com>
2016-04-12 15:18:52 -0700

    Complete Release 0

---
+++ js/data_structures.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "red", "green", "orange"];
+  
+var horseNames = ["Tony", "Brevit", "Bojack", "Ed"];
+
+colors.push("yellow")
+horseNames.push("Joe")
+
+console.log(colors, horseNames)
