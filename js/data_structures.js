75fb9f0eac66b5c057e613d00bcf8373240576e6
Jacqueline Phan <jtphan14@gmail.com>
2016-04-12 19:54:15 -0700

    Finish 7.2

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,30 +1,32 @@
 //Release 0
-var colors = ["blue", "red", "green", "orange"];
-  
-var horseNames = ["Tony", "Brevit", "Bojack", "Ed"];
 
+//Arrays
+var colors = ["blue", "red", "green", "orange"]; 
+var horseNames = ["Tony", "Brevit", "Bojack", "Ed"];
+//Add color and horse name to arrays
 colors.push("yellow")
 horseNames.push("Joe")
-
+//Print out color and Horse Arrays
 console.log(colors, horseNames)
 
 //Release 1
+//Create Empty Array
 var matcher = {};
-
+//For Loop to add horse and color 
 for (var i = 0; i < horseNames.length; i++) {
   matcher[horseNames[i]] = colors[i]}
-  
+//Print out updated matcher object
 console.log(matcher)
 
 //Release 2
+//Constructor Function
 function Car(model,color,isAutomatic) {
   console.log("On the assembly line:", this)
-
-  
+//Create "attributes" for *this  
   this.model = model;
   this.color = color;
   this.isAutomatic = isAutomatic;
-  
+ //Create Shift Function that is an if/else statement 
   this.shift = function() {
     if(this.isAutomatic) {
       console.log("Enjoy driving right now!");
@@ -32,12 +34,12 @@ function Car(model,color,isAutomatic) {
       console.log("Back to driving school to learn how to drive stick!");
     }
   };  
-  
+  //Create simple function
   this.horn = function() {
     console.log("beep beep")
   };
 }
-
+//Driver Code
 var alexCar = new Car("Honda","Blue", false);
 console.log(alexCar)
 alexCar.horn();
@@ -47,49 +49,3 @@ var jaqCar = new Car("Audi", "Black", true);
 console.log(jaqCar);
 jaqCar.shift()
 
-
-// var horse = (horseNames[0])
-
-// console.log (horseNames[1])
-
-// matcher.horse = "purple"
-
-// console.log(matcher)
-// 
-// matcher[horseNames[1]] = colors[1]
-// 
-// console.log(matcher)
-// 
-// 
-// for (var i = 0; i < horseNames.length; i++) {
-  // matcher[horseNames[i]] = colors[i]}
-//   
-// console.log(matcher)
-// 
-// 
-// for (var i = 0; i < kittens.length; i++) {
-//   console.log(kittens[i]);
-
-
-// // say hello
-
-
-// Let's declare an object:
-// var dog = {name: 'Spot', 'age': 3, isGoodDog: true};
-
-// Object values can be accessed with bracket notation ...
-// console.log("The dog's name is " + dog['name'] + ".");
-// 
-// or dot notation ...
-// console.log("The dog's name is " + dog.age + ".");
-// 
-// // to access information:
-// var propertyName = 'name';
-// console.log(dog[name]);
-// 
-// // You can add values anytime:
-// dog.adopted = false; // :(
-// 
-// // And update values anytime:
-// dog.adopted = true; // aww yay! :D
-// 
