99544b4c2792c1639464ba8d081338d35ab53b97
Jacqueline Phan <jtphan14@gmail.com>
2016-04-21 21:29:09 -0700

    Delete old database, create new database, and addd tables

--- databases/8.5/packing_list_creator.rb
+++ databases/8.5/packing_list_creator.rb
@@ -1,4 +1,4 @@
-#Packing List
+#Packing List (Inspired by my recent trip to Canada)
 #Create a ruby program that will help people create a list of items to pack for a vacation.
 #Ask how many days their trip is
 #Method to add items to the list with statement to let them if item is already on the list
@@ -12,19 +12,36 @@ require 'faker'
 #create SQlite3 database
 db = SQLite3::Database.new("packing_list.db")
 
-#create table method to store packing list
-create_table_cmd = <<-SQL
+#create table method to store items to pack
+create_items_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS list(
 	id INTEGER PRIMARY KEY,
 	item VARCHAR(255),
-	quantity INT
+	quantity INT,
+	category_id INT,
+	packed BOOLEAN,
+	FOREIGN KEY (category_id) REFERENCES categories(id)
 	)
 SQL
 
+create_categories_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS list(
+	id INTEGER PRIMARY KEY,
+	category_name VARCHAR(255)
+	)
+SQL
+#create category of items
+
+
 #create a packing list tables(if it's not there already)
-db.execute(create_table_cmd)
+db.execute(create_items_cmd)
+db.execute(create_categories_cmd)
 
 #Method to Add item to list
 def add_item(db, item, quantity)
-	db.execute("INSERT INTO list (item, quantity") VALUES (?, ?)", [item, quantity])"
+	db.execute("INSERT INTO list (item, quantity) VALUES (?, ?)", [item, quantity])
+end
+
+def remove_item(db,item,quantity, deleted_item)
+	db.execute("DELETE FROM list (item, quantity) WHERE item = deleted_items")
 end
