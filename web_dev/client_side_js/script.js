f64f5bf2bac5f4506fa26b9e4638c7985b006fb2
Jacqueline Phan <jtphan14@gmail.com>
2016-04-27 11:55:29 -0700

    Complete Release 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,8 +1,26 @@
 console.log("Testing");
 
-document.getElementById("footnote").style.border = "thick solid #0000FF";
-"thick solid #0000FF";
+// document.getElementById("footnote").style.border = "thick solid #0000FF";
+// "thick solid #0000FF";
 
-document.body.style.backgroundColor = "red";
 
 document.getElementById("aside").style.cursor = "pointer";
+
+function addBackgroundColor(event) {
+	event.target.style.backgroundColor = "red";
+}
+
+var background = document.body;
+background.addEventListener("click", addBackgroundColor);
+
+function footerBorder(event){
+	event.target.style.border = "thick solid #0000FF";
+}
+
+function removeFooterBorder(event){
+	event.target.style.border = "0px";
+}
+
+var border = document.getElementById("footnote");
+border.addEventListener("mouseover", footerBorder);
+border.addEventListener("mouseout", removeFooterBorder);
