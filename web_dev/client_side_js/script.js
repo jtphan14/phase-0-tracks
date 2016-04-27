3b6816d0d92664be0beab18664fc2d471e32d572
Jacqueline Phan <jtphan14@gmail.com>
2016-04-27 11:31:53 -0700

    Initial commit

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,8 @@
+console.log("Testing");
+
+document.getElementById("footnote").style.border = "thick solid #0000FF";
+"thick solid #0000FF";
+
+document.body.style.backgroundColor = "red";
+
+document.getElementById("aside").style.cursor = "pointer";
