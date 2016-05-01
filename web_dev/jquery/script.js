76aa9c4b69d02604a68ab5de1cd121e9fb1316bb
Jacqueline Phan <jtphan14@gmail.com>
2016-05-01 15:36:53 -0700

    Add button and add enlarge picture

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $('img').animate({
+            left: '250px',
+            height: '+=150px',
+            width: '+=150px'
+        });
+    });
+});
+
+// $(document).click(function(){
+//     $('img).hide("slow", function(){
+//         alert("The paragraph is now hidden");
+//     });
+// });
