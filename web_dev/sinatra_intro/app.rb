5839cb8255ac336e404bfc78fc8aa94522545848
Jacqueline Phan <jtphan14@gmail.com>
2016-04-30 17:18:37 -0700

    Complete first_name search

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -67,3 +67,22 @@ get '/:num_1/add/:num_2' do
   "The sum of #{params[:num_1]} and #{params[:num_2]} is #{sum}."
 end
 
+#write a GET route that allows user to search by first_name
+get '/student_search/:name' do
+  name = []
+  student = db.execute("SELECT * FROM students")
+  student.each do |student|
+    first_name = "#{student['name']}".split(' ')
+    name << first_name[0]
+  end
+  if name.include?(params[:name])
+    "yes"
+    # student = db.execute("SELECT * FROM students WHERE ")
+    # "ID: #{student['id']}<br>"
+    # "Name: #{student['name']}<br>"
+    # "Age: #{student['age']}<br>"
+    # "Campus: #{student['campus']}<br><br>"
+  else
+    "Sorry, there are no student's by that name"
+  end
+  end
